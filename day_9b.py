013253e3e8fd79504fcfdb1523f73d57f2a05859
Dino Viehland <dinoviehland@fb.com>
2023-12-08 22:30:44 -0800

    Day 9

---
+++ day_9b.py
@@ -0,0 +1,51 @@
+# ./python -X jit -m compiler --static aoc/day_9a.py
+from __future__ import annotations
+
+import __static__
+
+from __static__ import Array, box, cbool, CheckedDict, CheckedList, clen, crange, int64
+
+from typing import List
+
+
+def predict(histories: List[int]) -> int:
+    new_hist: List[int] = []
+    x = histories[0]
+    nonzero: bool = False
+    for i, y in enumerate(histories[1:]):
+        delta = y - x
+        if delta:
+            nonzero = True
+        new_hist.append(delta)
+        x = y
+
+    if nonzero:
+        new_val = predict(new_hist)
+        below = new_hist[0] - new_val
+        new_hist.insert(0, below)
+        print(new_hist)
+        return below
+    else:
+        return 0
+
+
+def run(example: cbool) -> None:
+    with open("aoc/data/9ex.txt" if example else "aoc/data/9.txt") as f:
+        lines = f.readlines()
+
+    histories = []
+    for line in lines:
+        histories.append([int(value) for value in line.strip().split(" ")])
+
+    tot = 0
+    for history in histories:
+        print("---", history)
+        new_val = predict(history)
+        print([history[0] - new_val] + history)
+        below = history[0] - new_val
+        tot += below
+
+    print(tot)
+
+
+run(False)
